4080869e8cd504ded017d140a2035b729c36bebe
AnoDelta <AnoDeltaz@gmail.com>
2026-03-25 03:20:05 +0800

    added ability to build for windows

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main() {
 	InitWindow(900,  800, "KonAkiEngine");
 
 	while (!WindowShouldClose()) {
-		ClearBackground(0.2f, 0.3f, 0.3f);
+		ClearBackground(0.2f, 0.0f, 0.0f);
 		Present();
 		PollEvents();
 	}
